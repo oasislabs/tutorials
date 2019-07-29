6977fa165244410b3d4ee9497fed3ed9b5abcdca
sharmaster96 <ennsharma@oasislabs.com>
2019-07-29 10:25:01 -0700

    Modify hello world

--- hello-world/README.md
+++ hello-world/README.md
@@ -1,1 +1,4 @@
-# hello-world
+# Oasis Quickstart
+
+This repository is a template for building services that run on the Oasis blockchain platform.
+You can clone it manually or create a fresh copy using `oasis init`.
--- hello-world/app/test/service.spec.ts
+++ hello-world/app/test/service.spec.ts
@@ -1,35 +1,14 @@
-const oasis = require('@oasislabs/client');
+import oasis from '@oasislabs/client';
+
+jest.setTimeout(20000);
 
 describe('HelloWorld Test', () => {
   it('should deploy non-confidentially', async () => {
-    const service = await oasis.project.HelloWorldClient.deploy({
-      confidential: false,
+    const service = await oasis.workspace.HelloWorld.deploy({
+      header: {confidential: false},
     });
 
     expect(service).toBeTruthy();
-  });
-
-  it('test known greeting', async () => {
-    let greeting = await service.say_hello("sl");
-    console.log("In slovenian: %s", greeting);
-
-    expect(greeting).toEqual("Pozdravljen, svet!");
-  });
-
-  it('test unknown greeting', async () => {
-    let greeting = await service.say_hello("ws");
-    console.log("In Samoan: %s", greeting);
-
-    expect(greeting).toBeNull();
-  });
-
-  it('insert new greeting in Samoan', async () => {
-    await service.add_hello("ws", "alofa fiafia i le lalolagi!");
-
-    let greeting = await service.say_hello("ws");
-    console.log("In Samaon: %s", greeting);
-
-    expect(greeting).toEqual("alofa fiafia i le lalolagi!");
   });
 
   afterAll(() => {
