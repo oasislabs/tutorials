d0ffb63a87eb7bbeb6d4d71de0495d4288ac493d
sharmaster96 <ennsharma@oasislabs.com>
2019-07-29 10:27:07 -0700

    adding tests

--- hello-world/app/test/service.spec.ts
+++ hello-world/app/test/service.spec.ts
@@ -11,6 +11,29 @@ describe('HelloWorld Test', () => {
     expect(service).toBeTruthy();
   });
 
+  it('test known greeting', async () => {
+    let greeting = await service.say_hello("sl");
+    console.log("In slovenian: %s", greeting);
+
+    expect(greeting).toEqual("Pozdravljen, svet!");
+  });
+
+  it('test unknown greeting', async () => {
+    let greeting = await service.say_hello("ws");
+    console.log("In Samoan: %s", greeting);
+
+    expect(greeting).toBeNull();
+  });
+
+  it('insert new greeting in Samoan', async () => {
+    await service.add_hello("ws", "alofa fiafia i le lalolagi!");
+
+    let greeting = await service.say_hello("ws");
+    console.log("In Samaon: %s", greeting);
+
+    expect(greeting).toEqual("alofa fiafia i le lalolagi!");
+  });
+
   afterAll(() => {
     oasis.disconnect();
   });
