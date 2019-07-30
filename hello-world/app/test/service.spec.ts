9116ec61e5b370a8c879dce1c9a7aa2df7364d41
vishwa-raman <vishwar@oasislabs.com>
2019-07-30 08:39:50 -0700

    Hello world tests to use jest

--- hello-world/app/test/service.spec.ts
+++ hello-world/app/test/service.spec.ts
@@ -3,35 +3,29 @@ import oasis from '@oasislabs/client';
 jest.setTimeout(20000);
 
 describe('HelloWorld Test', () => {
-  it('should deploy non-confidentially', async () => {
-    const service = await oasis.workspace.HelloWorld.deploy({
+  let service;
+
+  beforeAll(async () => {
+    service = await oasis.workspace.HelloWorld.deploy({
       header: {confidential: false},
     });
+  });
 
+  it('deployed', async () => {
     expect(service).toBeTruthy();
   });
 
-  it('test known greeting', async () => {
-    let greeting = await service.say_hello("sl");
-    console.log("In slovenian: %s", greeting);
-
-    expect(greeting).toEqual("Pozdravljen, svet!");
-  });
-
-  it('test unknown greeting', async () => {
-    let greeting = await service.say_hello("ws");
-    console.log("In Samoan: %s", greeting);
-
-    expect(greeting).toBeNull();
+  it('known greeting', async () => {
+    let greeting = await service.say_hello('sl');
+    expect(greeting).toEqual('Pozdravljen, svet!');
   });
 
   it('insert new greeting in Samoan', async () => {
-    await service.add_hello("ws", "alofa fiafia i le lalolagi!");
-
-    let greeting = await service.say_hello("ws");
-    console.log("In Samaon: %s", greeting);
-
-    expect(greeting).toEqual("alofa fiafia i le lalolagi!");
+    let greeting = await service.say_hello('ws');
+    expect(greeting).toBeNull();
+    await service.add_hello('ws', 'alofa fiafia i le lalolagi!');
+    greeting = await service.say_hello('ws');
+    expect(greeting).toEqual('alofa fiafia i le lalolagi!');
   });
 
   afterAll(() => {
