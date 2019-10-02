6ed8364e636f15a143666c0667602cefcd74a95f
Gloria Zhao <gzhao408@berkeley.edu>
2019-10-02 14:22:05 -0700

    changed so it works on devnet

--- rock-paper-scissors/service/src/main.rs
+++ rock-paper-scissors/service/src/main.rs
@@ -2,106 +2,92 @@ use oasis_std::{Address, Context};
 
 #[derive(oasis_std::Service)]
 struct RockPaperScissors {
-    player: Address,
-    player_move: u32, // the first player's move is stored. the challenger's isn't.
+    player_name: String,
+    player_move: u32, // TODO would be better as an enum?
     // 1 = rock, 2 = paper, 3 = scissors
+    challenger_name: String,
     challenger_move: u32,
     // 1 = rock, 2 = paper, 3 = scissors
     open: bool, // whether or not someone can challenge. true = player has played. false = nobody has played or just finished.
-    won: u32,
-    lost: u32,
-    tied: u32,
 }
 
 
 type Result<T> = std::result::Result<T, String>;
 
-
 impl RockPaperScissors {
 
-    pub fn new(ctx: &Context) -> Self {
+    pub fn new(_ctx: &Context) -> Self {
         Self {
-            player: ctx.sender(),
+            player_name: "".to_string(),
             player_move: 0,
+            challenger_name: "".to_string(),
             challenger_move: 0,
-            open: false,
-            won: 0,
-            lost: 0,
-            tied: 0
+            open: true,
         }
 
     }
 
-    pub fn can_challenge(&self, _ctx: &Context) -> bool {
+    pub fn can_play(&self, _ctx: &Context) -> bool {
         return self.open;
     }
 
     pub fn reveal(&self, _ctx: &Context) -> Result<String> {
         let map = vec!["None".to_string(), "Rock".to_string(), "Paper".to_string(), "Scissors".to_string()];
         if self.open {
-            return Err("The current game hasn't finished yet. You can query scores by calling scores().".to_string())
-        }
-        if self.player_move <= 0 {
-            return Err("No games have been played yet!".to_string());
+            return Err("The game hasn't finished yet. You can query scores by calling scores().".to_string())
         }
         if self.player_move == self.challenger_move {
-            let x: String = format!("Player played {} and Challenger played {}. Tie!", map[self.player_move as usize], map[self.challenger_move as usize]);
+            let x: String = format!("{} played {} and {} played {}. Tie!", self.player_name, map[self.player_move as usize], self.challenger_name, map[self.challenger_move as usize]);
             return Ok(x);
         }
         if self.player_move > self.challenger_move {
-            let x: String = format!("Player played {} and Challenger played {}. Player Wins!", map[self.player_move as usize], map[self.challenger_move as usize]);
+            let x: String = format!("{} played {} and {} played {}. Player Wins!", self.player_name, map[self.player_move as usize], self.challenger_name, map[self.challenger_move as usize]);
             return Ok(x);
         }
         if self.player_move == 1 && self.challenger_move == 3 {
-            let x: String = format!("Player played {} and Challenger played {}. Player Wins!", map[self.player_move as usize], map[self.challenger_move as usize]);
+            let x: String = format!("{} played {} and {} played {}. Player Wins!", self.player_name, map[self.player_move as usize], self.challenger_name, map[self.challenger_move as usize]);
             return Ok(x);
         }
-        let x: String = format!("Player played {} and Challenger played {}. Challenger Wins!", map[self.player_move as usize], map[self.challenger_move as usize]);
+        let x: String = format!("{} played {} and {} played {}. Challenger Wins!", self.player_name, map[self.player_move as usize], self.challenger_name, map[self.challenger_move as usize]);
         return Ok(x);
     }
 
-    pub fn scores(&self, _ctx: &Context) -> String {
-        return format!("Won: {}, Lost: {}, and Tied: {}", self.won, self.lost, self.tied);
-    }
-
-    pub fn play(&mut self, ctx: &Context, p_move: u32) -> Result<String> {
-        if self.player != ctx.sender() {
-            return Err("You aren't the main player. Start a new game!".to_string());
+    pub fn play(&mut self, _ctx: &Context, p_name: String, p_move: u32) -> Result<String> {
+        if !self.open {
+            return Err("The game has ended.".to_string());
+        }
+        if self.player_move > 0 {
+            return Err("Someone already played.".to_string());
         }
         if p_move < 1 || p_move > 3 {
             return Err("Invalid move.".to_string());
         }
         self.player_move = p_move;
-        self.open = true; // player always has to go first, otherwise we'll get confused
-        Ok("Player has played. The move is secret until the Challenger plays.".to_string())
+        self.player_name = p_name;
+        if self.player_move > 0 && self.challenger_move > 0 {
+            self.open = false;
+            return Ok("Both players have played. Use reveal() to see what both players' moves were.".to_string());
+        }
+        Ok(format!("Player {} has played.", self.player_name))
     }
 
-    pub fn challenge(&mut self, ctx: &Context, c_move: u32) -> Result<String> {
-        if self.player == ctx.sender() {
-            return Err("You can't play against yourself.".to_string());
-        }
+    pub fn challenge(&mut self, _ctx: &Context, c_name: String, c_move: u32) -> Result<String> {
         if !self.open {
-            return Err("Player hasn't started a new game yet.".to_string());
+            return Err("The game has ended.".to_string());
+        }
+        if self.challenger_move > 0 {
+            return Err("Someone already challenged.".to_string());
         }
         if c_move < 1 || c_move > 3 {
             return Err("Invalid move.".to_string());
         }
         self.challenger_move = c_move;
-
-        if self.player_move == self.challenger_move {
-            self.tied += 1;
+        self.challenger_name = c_name;
+        if self.player_move > 0 && self.challenger_move > 0 {
+            self.open = false;
+            return Ok("Both players have played. Use reveal() to see what both players' moves were.".to_string());
         }
-        else if self.player_move > self.challenger_move {
-            self.won += 1;
-        }
-        else if self.player_move == 1 && self.challenger_move == 3 {
-            self.won += 1;
-        }
-        else {
-            self.lost += 1;
-        }
-        self.open = false;
-        Ok("Challenger has played. Use reveal() to see what both players' moves were.".to_string())
+        Ok(format!("Player {} has played.", self.challenger_name))
     }
 }
 
@@ -128,24 +114,23 @@ mod tests {
         let (_player, player_ctx) = create_account();
         let (_challenger, challenger_ctx) = create_account();
 
+
         let mut game = RockPaperScissors::new(&player_ctx);
 
-        assert_eq!(game.can_challenge(&player_ctx), false);
-        // assert_eq!(game.scores(&player_ctx), "Won: 0, Lost: 0, and Tied: 0".to_string());
+        assert_eq!(game.can_play(&player_ctx), true); // they can play in any order tbh
 
         // player 1 plays rock
-        game.play(&player_ctx, 1).unwrap();
+        println!("{}", game.play(&player_ctx, "stan".to_string(), 1).unwrap());
 
         // anyone can now challenge
-        assert_eq!(game.can_challenge(&player_ctx), true);
+        assert_eq!(game.can_play(&player_ctx), true);
 
         // challenger challenges, plays rock
-        game.challenge(&challenger_ctx, 1).unwrap();
+        println!("{}", game.challenge(&challenger_ctx, "nick".to_string(), 1).unwrap());
 
         // nobody can challenge anymore
-        assert_eq!(game.can_challenge(&player_ctx), false);
+        assert_eq!(game.can_play(&player_ctx), false);
 
         println!("{}", game.reveal(&player_ctx).unwrap());
-        println!("{}", game.scores(&player_ctx));
     }
 }
