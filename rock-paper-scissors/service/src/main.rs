78f1b72cab4a6d96d93d4dfd1382c7fc70833449
Gloria Zhao <gzhao408@berkeley.edu>
2019-10-02 11:32:28 -0700

    basic functionality test, passing

--- rock-paper-scissors/service/src/main.rs
+++ rock-paper-scissors/service/src/main.rs
@@ -64,7 +64,7 @@ impl RockPaperScissors {
         return format!("Won: {}, Lost: {}, and Tied: {}", self.won, self.lost, self.tied);
     }
 
-    pub fn play(&mut self, ctx: &Context, p_move: u32) -> Result<()> {
+    pub fn play(&mut self, ctx: &Context, p_move: u32) -> Result<String> {
         if self.player != ctx.sender() {
             return Err("You aren't the main player. Start a new game!".to_string());
         }
@@ -73,10 +73,10 @@ impl RockPaperScissors {
         }
         self.player_move = p_move;
         self.open = true; // player always has to go first, otherwise we'll get confused
-        Ok(())
+        Ok("Player has played. The move is secret until the Challenger plays.".to_string())
     }
 
-    pub fn challenge(&mut self, ctx: &Context, c_move: u32) -> Result<()> {
+    pub fn challenge(&mut self, ctx: &Context, c_move: u32) -> Result<String> {
         if self.player == ctx.sender() {
             return Err("You can't play against yourself.".to_string());
         }
@@ -101,11 +101,51 @@ impl RockPaperScissors {
             self.lost += 1;
         }
         self.open = false;
-        Ok(())
+        Ok("Challenger has played. Use reveal() to see what both players' moves were.".to_string())
     }
 }
 
 
 fn main() {
     oasis_std::service!(RockPaperScissors);
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate oasis_test;
+
+    use super::*;
+
+
+    fn create_account() -> (Address, Context) {
+        let sender = oasis_test::create_account(1);
+        let ctx = Context::default().with_sender(sender);
+        (sender, ctx)
+    }
+
+    #[test]
+    fn functionality() {
+        let (_player, player_ctx) = create_account();
+        let (_challenger, challenger_ctx) = create_account();
+
+        let mut game = RockPaperScissors::new(&player_ctx);
+
+        assert_eq!(game.can_challenge(&player_ctx), false);
+        // assert_eq!(game.scores(&player_ctx), "Won: 0, Lost: 0, and Tied: 0".to_string());
+
+        // player 1 plays rock
+        game.play(&player_ctx, 1).unwrap();
+
+        // anyone can now challenge
+        assert_eq!(game.can_challenge(&player_ctx), true);
+
+        // challenger challenges, plays rock
+        game.challenge(&challenger_ctx, 1).unwrap();
+
+        // nobody can challenge anymore
+        assert_eq!(game.can_challenge(&player_ctx), false);
+
+        println!("{}", game.reveal(&player_ctx).unwrap());
+        println!("{}", game.scores(&player_ctx));
+    }
 }
