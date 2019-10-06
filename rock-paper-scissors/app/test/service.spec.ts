d77591eefa194a94e0ca84dddffd104442fd87e0
Gloria Zhao <gzhao408@berkeley.edu>
2019-10-06 16:30:48 -0700

    fixed client side test

--- rock-paper-scissors/app/test/service.spec.ts
+++ rock-paper-scissors/app/test/service.spec.ts
@@ -19,18 +19,15 @@ describe('RockPaperScissors', () => {
     let canplay = await service.canPlay();
     expect(canplay).toEqual(true);
     let player1 = await service.play("stan", 3);
-    console.log(player1);
     expect(player1).toEqual('Player stan has played.');
 
 
     let player2 = await service.challenge("nick", 3);
-    console.log(player2);
     expect(player2).toEqual("Both players have played. Use reveal() to see what both players' moves were.");
 
     let canplay2 = await service.canPlay();
     expect(canplay2).toEqual(false);
     let score = await service.reveal();
-    console.log(score);
     expect(score).toEqual('stan played Scissors and nick played Scissors. Tie!');
   })
 
