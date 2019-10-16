696d3283f00745e98e1f06506054f080222db9a7
Fang <dfangshuo@oasislabs.com>
2019-10-16 14:07:46 -0700

    squashed dice game commit

---
+++ dice-game/app/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'plugins': [
+        '@typescript-eslint'
+    ],
+};
